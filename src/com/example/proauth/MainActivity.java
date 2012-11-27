9a0c2b597240b6f5e58e64dbc050b78b24639937
Joy Chen <drgnfnix@gmail.com>
2012-11-27 11:57:18 -0500

    Preferences Changes. Main menu mock. \n\n Made a menu with mocks of preferences. a demonstration of how to access preferences is in the main activity.

--- src/com/example/proauth/MainActivity.java
+++ src/com/example/proauth/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.proauth;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	//CheckBox prefCheckBox;
+	TextView prefManageApps;
+	ListView prefList;
+	String TAG = "MainActivity";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+        // For the TextView
+        //prefCheckBox = (CheckBox)findViewById(R.id.prefCheckBox);
+    	prefManageApps = (TextView)findViewById(R.id.prefEditText);
+    	loadPref();
+    	
+    	// For the ListView
+    	prefList = (ListView) findViewById(R.id.prefList);
+    	String[] values = new String[] {"Manage Your Apps", "History and Logs", "FAQ & Tutorial", 
+    			"About ProAuth"
+    	};
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+    			  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+    	prefList.setAdapter(adapter);
     }
 
     @Override
@@ -19,4 +48,44 @@ public class MainActivity extends Activity {
         return true;
     }
     
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		/*
+		 * Because it's only ONE option in the menu.
+		 * In order to make it simple, We always start SetPreferenceActivity
+		 * without checking.
+		 */
+		
+		Intent intent = new Intent();
+        intent.setClass(MainActivity.this, SetPreferencesActivity.class);
+        startActivityForResult(intent, 0); 
+		
+        return true;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		//super.onActivityResult(requestCode, resultCode, data);
+		
+		/*
+		 * To make it simple, always re-load Preference setting.
+		 */
+		
+		loadPref();
+	}
+
+	private void loadPref(){
+		SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		boolean my_checkbox_preference = mySharedPreferences.getBoolean("security_level_0", false);
+		Log.d(TAG, "Checkbox pref:" + my_checkbox_preference);
+		//prefCheckBox.setChecked(my_checkbox_preference);
+
+		String my_edittext_preference = mySharedPreferences.getString("edittext_preference", "");
+    	prefManageApps.setText(my_edittext_preference);
+
+	}
 }
