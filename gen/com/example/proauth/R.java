d8ed2dc7d092c6819d2c38e7edb5dfb66a197b85
Joy Chen <drgnfnix@gmail.com>
2012-12-07 18:15:16 -0500

    lots of things that didn't work :( function of last two buttons have been redirected

--- gen/com/example/proauth/R.java
+++ gen/com/example/proauth/R.java
@@ -19,29 +19,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int appList=0x7f090005;
-        public static final int app_icon=0x7f090002;
-        public static final int app_name=0x7f090003;
-        public static final int app_security_level=0x7f090004;
-        public static final int menu_settings=0x7f090006;
-        public static final int prefEditText=0x7f090000;
-        public static final int prefList=0x7f090001;
+        public static final int appList=0x7f090007;
+        public static final int app_icon=0x7f090004;
+        public static final int app_name=0x7f090005;
+        public static final int app_security_level=0x7f090006;
+        public static final int faq_content=0x7f090000;
+        public static final int main_page_welcome=0x7f090001;
+        public static final int menu_settings=0x7f090008;
+        public static final int prefEditText=0x7f090002;
+        public static final int prefList=0x7f090003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int app_security_listitem=0x7f030001;
-        public static final int manage_apps=0x7f030002;
+        public static final int activity_faq=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int app_security_listitem=0x7f030002;
+        public static final int manage_apps=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
-        public static final int dummy=0x7f060006;
+        public static final int dummy=0x7f060007;
         public static final int hello_world=0x7f060001;
         public static final int hello_world_main=0x7f060002;
         public static final int manage_apps=0x7f060005;
         public static final int menu_settings=0x7f060003;
+        public static final int title_activity_faq=0x7f060006;
         public static final int title_activity_main=0x7f060004;
     }
     public static final class style {
--- src/com/example/proauth/MainActivity.java
+++ src/com/example/proauth/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.proauth;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -20,94 +29,137 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	//CheckBox prefCheckBox;
+	// CheckBox prefCheckBox;
 	TextView prefManageApps;
 	ListView prefList;
 	String TAG = "MainActivity";
-	String[] values = new String[] {"Manage Your Apps", "History and Logs", "FAQ & Tutorial", 
-			"About ProAuth"
-	};
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ActionBar ab = getActionBar(); 
-        ab.setDisplayShowTitleEnabled(false); 
-        ab.setDisplayShowHomeEnabled(false);
-        setContentView(R.layout.activity_main);
+	String[] values = new String[] { "Manage Your Apps", "History and Logs",
+			"FAQ & Tutorial", "About ProAuth" };
 
-        
-        // For the TextView
-        //prefCheckBox = (CheckBox)findViewById(R.id.prefCheckBox);
-    	prefManageApps = (TextView)findViewById(R.id.prefEditText);
-    	loadPref();
-    	
-    	// For the ListView
-    	prefList = (ListView) findViewById(R.id.prefList);
-    	
-    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-    			  android.R.layout.simple_list_item_1, android.R.id.text1, values);
-    	prefList.setAdapter(adapter);
-    	OnItemClickListener listener = new OnItemClickListener(){
-    	      public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
-    	          String selectedFromList =(String) (prefList.getItemAtPosition(myItemInt));
-    	          Log.d(TAG, selectedFromList);
-    	          if (selectedFromList.equals(values[0])){
-        	          Intent intent = new Intent("com.example.proauth.ManageAppsActivity"); 
-        	          startActivity(intent); 
-    	          };
-    	        }
-    	};
-    	prefList.setOnItemClickListener(listener);
-    }
+	FileOutputStream log_file;
+	public static String LOG_FILE = "proauth_log.txt";
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ActionBar ab = getActionBar();
+		ab.setDisplayShowTitleEnabled(false);
+		ab.setDisplayShowHomeEnabled(false);
+		setContentView(R.layout.activity_main);
+
+		Log.e(TAG, this.getApplicationContext().getFilesDir().toString());
+		try {
+			log_file = this.getApplicationContext().openFileOutput(LOG_FILE, 0);
+
+		} catch (FileNotFoundException e2) {
+			e2.printStackTrace();
+		}
+
+		// For the TextView
+		// prefCheckBox = (CheckBox)findViewById(R.id.prefCheckBox);
+		prefManageApps = (TextView) findViewById(R.id.prefEditText);
+		loadPref();
+
+		// For the ListView
+		prefList = (ListView) findViewById(R.id.prefList);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		prefList.setAdapter(adapter);
+		OnItemClickListener listener = new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> myAdapter, View myView,
+					int myItemInt, long mylng) {
+				String selectedFromList = (String) (prefList
+						.getItemAtPosition(myItemInt));
+				Log.d(TAG, selectedFromList);
+				if (selectedFromList.equals(values[0])) {
+					Intent intent = new Intent(
+							"com.example.proauth.ManageAppsActivity");
+					startActivity(intent);
+				} else if (selectedFromList.equals(values[3])) {
+					try {
+						Process process = Runtime.getRuntime()
+								.exec("logcat ActivityManager:I *:S");
+						InputStreamReader isr = new InputStreamReader(
+								process.getInputStream());
+						BufferedReader bufferedReader = new BufferedReader(isr);
+
+						StringBuilder log = new StringBuilder();
+						String line;
+						while ((line = bufferedReader.readLine()) != null) {
+							if (!line.contains("MainActivity")) {
+								log.append(line);
+							}
+						}
+						
+						//Log.e("LOGCAT CLONE", log.toString());
+						
+						TextView tv = (TextView) findViewById(R.id.main_page_welcome);
+						tv.setText(log.toString());
+					} catch (IOException e) {
+						Log.e(TAG, e.getMessage());
+					}
+					// Intent intent = new
+					// Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+					// intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+					// securemeAdmin);
+				} else if (selectedFromList.equals(values[2])) {
+					Intent intent = new Intent(
+							"com.example.proauth.FAQActivity");
+					startActivity(intent);
+				}
+
+			}
+		};
+		prefList.setOnItemClickListener(listener);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
 		/*
-		 * Because it's only ONE option in the menu.
-		 * In order to make it simple, We always start SetPreferenceActivity
-		 * without checking.
+		 * Because it's only ONE option in the menu. In order to make it simple,
+		 * We always start SetPreferenceActivity without checking.
 		 */
-		
+
 		Intent intent = new Intent();
-        intent.setClass(MainActivity.this, SetPreferencesActivity.class);
-        startActivityForResult(intent, 0); 
-		
-        return true;
+		intent.setClass(MainActivity.this, SetPreferencesActivity.class);
+		startActivityForResult(intent, 0);
+
+		return true;
 	}
-	
-	
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
-		//super.onActivityResult(requestCode, resultCode, data);
-		
+		// super.onActivityResult(requestCode, resultCode, data);
+
 		/*
 		 * To make it simple, always re-load Preference setting.
 		 */
-		
+
 		loadPref();
 	}
 
-	private void loadPref(){
-		SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-		
-		boolean my_checkbox_preference = mySharedPreferences.getBoolean("security_level_0", false);
-		Log.d(TAG, "Checkbox pref:" + my_checkbox_preference);
-		//prefCheckBox.setChecked(my_checkbox_preference);
+	private void loadPref() {
+		SharedPreferences mySharedPreferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
 
-		String my_edittext_preference = mySharedPreferences.getString("edittext_preference", "");
-    	prefManageApps.setText(my_edittext_preference);
+		boolean my_checkbox_preference = mySharedPreferences.getBoolean(
+				"security_level_0", false);
+		Log.d(TAG, "Checkbox pref:" + my_checkbox_preference);
+		// prefCheckBox.setChecked(my_checkbox_preference);
+
+		String my_edittext_preference = mySharedPreferences.getString(
+				"edittext_preference", "");
+		prefManageApps.setText(my_edittext_preference);
 
 	}
 }
