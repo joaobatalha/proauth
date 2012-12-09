0184254ab079089cb9e2a478d66d561b0432a34f
Joy Chen <drgnfnix@gmail.com>
2012-12-09 17:57:09 -0500

    merged with joao all files

---
+++ src/com/example/proauth/BlockActivityHandler.java
@@ -0,0 +1,84 @@
+package com.example.proauth;
+
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+
+public class BlockActivityHandler {
+	private Context current_context;
+	private String lockActivityName;
+	private String lastRunningPackage;
+	private ActivityManager activity_manager;
+	
+
+	public BlockActivityHandler(Context context) {
+		current_context = context;
+		lockActivityName = ".LockActivity";
+		activity_manager = (ActivityManager)current_context.getSystemService(Context.ACTIVITY_SERVICE);
+		lastRunningPackage = getRunningPackage();
+		
+		context.registerReceiver(new BroadcastReceiver(){
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				String packagename = intent.getStringExtra(LockScreenActivity.PACKAGE_NAME);
+				lastRunningPackage = packagename;
+			}
+		}, new IntentFilter(LockScreenActivity.PASSED));
+	}
+	
+	private String getRunningPackage(){
+		List<RunningTaskInfo> infos = activity_manager.getRunningTasks(1);
+		if (infos.size()<1) return null; 
+		RunningTaskInfo info = infos.get(0);
+		return info.topActivity.getPackageName();
+	}
+
+	public void onActivityStarting(String packageName, String activityName) {
+		synchronized (this) {
+			// Here we do not want to block the password activity
+			// But we do want to block the Preferences activity
+			if (packageName.equals(current_context.getPackageName())) {
+				if (activityName.equals(lockActivityName))
+					return;
+				blockActivity(packageName, activityName);
+			}
+//			if (packageName.equals(lastRunningPackage)) return;
+			// Here we are going to want to go through the list of blocked apps
+			// And check if the packageName is in that list
+//			Log.d("JOAO", "packageNames variable" + packageName);
+			if (packageName.equals("com.example.myfirstapp")) {
+				lastRunningPackage = packageName;
+				return;
+			} else {
+//				if(packageName.equals("com.android.browser")){
+//					Log.d("JOAO","B");
+//					blockActivity(packageName, activityName);
+//					return;
+//				}
+				blockActivity(packageName, activityName);
+				return;
+//				lastRunningPackage = packageName;
+//				Log.d("JOAO", "Last running package: " + lastRunningPackage);
+			}
+		}
+	}
+
+	private void blockActivity(String packageName, String activityName) {
+		Log.i("Detector", "Blocking: " + packageName);
+
+		Intent locking_intent = new Intent(current_context, LockScreenActivity.class);
+		locking_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		locking_intent.putExtra(LockScreenActivity.BlockedActivityName, activityName);
+		locking_intent.putExtra(LockScreenActivity.BlockedPackageName, packageName);
+
+		current_context.startActivity(locking_intent);
+	}
+}
---
+++ src/com/example/proauth/MonitorService.java
@@ -0,0 +1,212 @@
+package com.example.proauth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RecentTaskInfo;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MonitorService extends Service {
+    private static final Class[] mStartForegroundSignature = new Class[] {
+        int.class, Notification.class};
+    private static final Class[] mStopForegroundSignature = new Class[] {
+        boolean.class};
+    
+    private NotificationManager mNM;
+    private Method mStartForeground;
+    private Method mStopForeground;
+    private Object[] mStartForegroundArgs = new Object[2];
+    private Object[] mStopForegroundArgs = new Object[1];
+    
+    @Override
+    public void onCreate() {
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        try {
+            mStartForeground = getClass().getMethod("startForeground",
+                    mStartForegroundSignature);
+            mStopForeground = getClass().getMethod("stopForeground",
+                    mStopForegroundSignature);
+        } catch (NoSuchMethodException e) {
+            // Running on an older platform.
+            mStartForeground = mStopForeground = null;
+        }
+        
+        if(init == 0){
+        	init = 1;
+        	get_log_command = getResources().getString(R.string.get_log_command);
+    		clear_log_command = getResources().getString(R.string.clear_log_command);
+    		String regex_pattern = getResources().getString(R.string.activity_name_pattern);
+    		ActivityPattern = Pattern.compile(regex_pattern,Pattern.CASE_INSENSITIVE);
+    		Log.d("Detector Service: ", "Initialized detector with pattern: " + regex_pattern);        
+        }	
+    }
+
+    /**
+     * This is a wrapper around the new startForeground method, using the older
+     * APIs if it is not available.
+     */
+    void startForegroundCompat(int id, Notification notification) {
+        // If we have the new startForeground API, then use it.
+        if (mStartForeground != null) {
+            mStartForegroundArgs[0] = Integer.valueOf(id);
+            mStartForegroundArgs[1] = notification;
+            try {
+                mStartForeground.invoke(this, mStartForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("MyApp", "Unable to invoke startForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("MyApp", "Unable to invoke startForeground", e);
+            }
+            return;
+        }
+        
+        // Fall back on the old API.
+        //setForeground(true);
+        mNM.notify(id, notification);
+    }
+    
+    /**
+     * This is a wrapper around the new stopForeground method, using the older
+     * APIs if it is not available.
+     */
+    void stopForegroundCompat(int id) {
+        // If we have the new stopForeground API, then use it.
+        if (mStopForeground != null) {
+            mStopForegroundArgs[0] = Boolean.TRUE;
+            try {
+                mStopForeground.invoke(this, mStopForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+                Log.w("MyApp", "Unable to invoke stopForeground", e);
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+                Log.w("MyApp", "Unable to invoke stopForeground", e);
+            }
+            return;
+        }
+        
+        mNM.cancel(id);
+        //setForeground(false);
+    }
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	private static Thread log_monitor_thread;
+	private static String get_log_command;
+	private static String clear_log_command;
+	private static Pattern ActivityPattern;
+	private static int init;
+	
+	//Code to execute when service is started 
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId){
+		if(log_monitor_thread != null){
+			log_monitor_thread.interrupt();
+		}
+		
+		CharSequence text = getText(R.string.service_running);
+		Notification note = new Notification(0,text,System.currentTimeMillis());
+		startForegroundCompat(R.string.service_running, note);
+		log_monitor_thread = new LogMonitoringThread(new BlockActivityHandler(this));
+		log_monitor_thread.start();
+		return Service.START_STICKY;
+		
+	}
+	
+	private String getActiveAppName(){
+		ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+		List<RecentTaskInfo> l = am.getRecentTasks(4, ActivityManager.RECENT_WITH_EXCLUDED);
+		Log.d("JOYC", l.get(0).baseIntent.toString());
+		Iterator i = l.iterator();
+		PackageManager pm = this.getPackageManager();
+		while (i.hasNext()) {
+		    ActivityManager.RecentTaskInfo info = (ActivityManager.RecentTaskInfo)(i.next());
+		    try {
+		    	Log.d("JOYC", info.origActivity.flattenToString());
+		        return info.baseIntent.toString();
+		    } catch (Exception e) {
+		        // Name Not FOund Exception
+		    }
+		}
+		return "<none>";
+	}
+	
+	private class LogMonitoringThread extends Thread{
+
+    	BufferedReader br;
+    	BlockActivityHandler blocking_handler;
+    	public LogMonitoringThread(BlockActivityHandler handler){
+    		blocking_handler = handler;
+    	}
+    	
+		@Override
+		public void run() {
+			Log.d("JOAO", "Starting Monitoring Thread");	
+			try {
+	    		Process process;
+	    		process = Runtime.getRuntime().exec(clear_log_command);
+				process = Runtime.getRuntime().exec(get_log_command);
+				br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+
+				while(( (line=br.readLine()) != null) && !this.isInterrupted()){
+
+					Log.d("JOAO", "Line read from the log: " + line);	
+					Log.d("JOAO", "Currently active app: " + getActiveAppName());	
+					if (line.contains("cat=[" + Intent.CATEGORY_HOME + "]")){
+						Log.d("JOAO", "Cat match");	
+						continue;
+					} 
+					
+					Matcher m = ActivityPattern.matcher(line);
+					
+					if (!m.find()){
+						Log.d("JOAO", "No match");	
+						continue;
+					}
+					
+					if (m.groupCount()<2){
+						Log.d("Detector Service: ", "Error while matching a line of the log.");
+						continue;
+					}
+					Log.d("JOAO", "Line matched in the log: " + line);	
+					Log.i("JOAO", "Found activity launching: " + m.group(1) + "  /   " + m.group(2));
+					if(blocking_handler != null){
+						blocking_handler.onActivityStarting(m.group(1), m.group(2));
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+    }
+	
+	@Override
+	public void onDestroy(){
+		log_monitor_thread.interrupt();
+		stopForegroundCompat(R.string.service_running);
+	}
+	
+}
